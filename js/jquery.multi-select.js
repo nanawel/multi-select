35d9df2bef288065b60ff047cdfcb1f1b24f0739
nanawel <nanawel@gmail.com>
2018-08-07 18:24:30 +0200

    Add AMD/Requirejs compatibility

--- js/jquery.multi-select.js
+++ js/jquery.multi-select.js
@@ -9,7 +9,39 @@
 * http://sam.zoy.org/wtfpl/COPYING for more details.
 */
 
-!function ($) {
+(function( factory ) {
+  "use strict";
+
+  if ( typeof define === 'function' && define.amd ) {
+    // AMD
+    define( ['jquery'], function ( $ ) {
+      return factory( $, window, document );
+    } );
+  }
+  else if ( typeof exports === 'object' ) {
+    // CommonJS
+    module.exports = function (root, $) {
+      if ( ! root ) {
+        // CommonJS environments without a window global must pass a
+        // root. This will give an error otherwise
+        root = window;
+      }
+
+      if ( ! $ ) {
+        $ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
+          require('jquery') :
+          require('jquery')( root );
+      }
+
+      return factory( $, root, root.document );
+    };
+  }
+  else {
+    // Browser
+    factory( jQuery, window, document );
+  }
+}
+(function ($, window, document) {
 
   "use strict";
 
@@ -539,6 +571,5 @@
         $parent.children().eq(index - 1).after(this);
       }
     });
-};
-
-}(window.jQuery);
+  }
+}));
